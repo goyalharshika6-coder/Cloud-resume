2983fb56e5a53067d06df788bf0e022c4f95db92
Harshika Goyal <goyalharshika6@gmail.com>
2026-06-14 01:18:37 +0530

    Added visitor counter using API Gateway Lambda and DynamoDB

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Cloud Resume Loaded");
+async function updateCounter() {
+    try {
+        const response = await fetch(
+            "https://bl3avdkbh9.execute-api.us-east-1.amazonaws.com/count"
+        );
+
+        const data = await response.json();
+
+        document.getElementById("visitor-count").innerText = data.count;
+
+    } catch (error) {
+        console.error("Error:", error);
+    }
+}
+
+updateCounter();
